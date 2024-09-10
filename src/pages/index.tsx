c70ae75985e843624bebe028868000cc084e1d32
mike dupont <mike.dupont@introspector.local>
2024-09-09 21:10:40 -0400

    include o1js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
+import { useEffect } from 'react';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  useEffect(() => {
+    (async () => {
+      const { Mina, PublicKey } = await import('o1js');
+      })();
+}, []);   
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
